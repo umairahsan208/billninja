f73f70bfb95f79f199e2eae607a0817b3673dfb8
Matatcoding <matthew_whitlock@comcast.net>
2025-11-06 19:51:26 -0500

    finally fount out that we were missing one line in server.js also changed a few things in th endpoints to check for errors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import app from "./app.js";
-import db from "./db/client.js"; // Import the client
+import db from "./db/client.js";
 
 const PORT = 3000;
 
 async function startServer() {
   try {
-    await db.connect(); // Connect to the database
+    await db.connect();
     console.log("Connected to the database");
 
     app.listen(PORT, () => {
